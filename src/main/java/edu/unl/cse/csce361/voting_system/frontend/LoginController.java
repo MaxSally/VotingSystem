69d7b611f2552791cb776eeed73b06651234846f
Max <qnguyen16@huskers.unl.edu>
2020-07-31 18:51:04 -0500

    Remove package car_rental and move files to frontend folder
    Not sure why it is there from previous commit.

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/LoginController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/LoginController.java
@@ -1,5 +1,6 @@
-package edu.unl.cse.csce361.car_rental.frontend;
+package edu.unl.cse.csce361.voting_system.frontend;
 
+import edu.unl.cse.csce361.voting_system.logic.DataLogic;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,17 +8,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Pane;
-import javafx.scene.control.Text;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
     @FXML private Pane paneLogin;
-    @FXML private java.awt.TextField txtUsername;
-    @FXML private java.awt.TextField txtPassword;
-    @FXML private java.awt.Text txtErrorText;
+    @FXML private TextField txtUsername;
+    @FXML private TextField txtPassword;
+    @FXML private Text txtErrorText;
 
     @FXML private Button btnLogin;
 
@@ -27,7 +28,7 @@ public class LoginController {
         String pass = txtPassword.getText();
 
         //use login method
-        if(login(userName, pass)){
+        if(DataLogic.getInstance().logIn(username, pass)){
             txtErrorText.setText("");
             navigate(event, "user_info.fxml");
         }
