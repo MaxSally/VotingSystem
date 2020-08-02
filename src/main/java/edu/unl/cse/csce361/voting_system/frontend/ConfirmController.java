424b12fc4f5adb36cf274d743247b33de8e8a14c
syeap2 <syeap2@huskers.unl.edu>
2020-08-02 15:05:11 -0500

    Added method to display questions and answer in frontend

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/ConfirmController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/ConfirmController.java
@@ -1,21 +1,64 @@
 package edu.unl.cse.csce361.voting_system.frontend;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ConfirmController extends ScreenController{
-    @FXML private Pane paneQuestions;
-    @FXML private Button btnCancel;
-    @FXML private Button btnSubmitVote;
-    @FXML private Button btnGoBack;
-
-    @FXML
-    private void initialize(){
-        //we'll dynamically add each question with answers the paneQuestions element (it scrolls)
-    }
+public class ConfirmController extends ScreenController implements Initializable{
+	
+	@FXML
+	private ListView<String> listView;
+	
+	@FXML
+	private Label userName;
+	
+	@FXML Label ssnValue;
+	
+	@FXML
+	private Label electionYear;
+	
+	ObservableList<String> data = FXCollections.observableArrayList();
+	
+	static class Cell extends ListCell<String>{
+		Label question = new Label();
+		ChoiceBox<String> answerList = new ChoiceBox<String>();
+		
+		public Cell() {
+			super();
+			HBox hbox = new HBox(question, answerList);
+			hbox.setSpacing(10);
+		}
+		
+		public void updateItem(String ballot, boolean empty) {
+			super.updateItem(ballot, empty);
+			setText(null);
+			setGraphic(null);
+			
+			if(ballot != null && !empty) {
+				question.setText("hi");
+				answerList.setValue("Select your choice");
+				//answerList.setItems(null); 
+			}
+		}
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		listView.setItems(data);
+		listView.setCellFactory(param -> new Cell());
+	}
 
     public void goBack(javafx.event.ActionEvent event) throws IOException{
         //all we should have to do here is navigate them back to the voting screen.
@@ -31,6 +74,8 @@ public class ConfirmController extends ScreenController{
         //cycle through each answer and submit vote with *submitVote()? method
         switchScreen(event, "thank_you.fxml");
     }
+
+	
 
 
 }
--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/VoteController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/VoteController.java
@@ -1,20 +1,58 @@
 package edu.unl.cse.csce361.voting_system.frontend;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class VoteController extends ScreenController{
-    @FXML private Pane paneQuestions;
-    @FXML private Button btnCancel;
-    @FXML private Button btnSubmitVote;
-
-    @FXML
-    private void initialize(){
-        //foreach question we'll dynamically add a text element and a dropdown (or radio buttons. dropdown might be easier) to the paneQuestions element (it scrolls)
-    }
+public class VoteController extends ScreenController implements Initializable{
+	@FXML
+	private ListView<String> listView;
+	
+	@FXML
+	private Label electionYear;
+	
+	ObservableList<String> data = FXCollections.observableArrayList();
+	
+	static class Cell extends ListCell<String>{
+		Label question = new Label();
+		ChoiceBox<String> answerList = new ChoiceBox<String>();
+		
+		public Cell() {
+			super();
+			HBox hbox = new HBox(question, answerList);
+			hbox.setSpacing(10);
+		}
+		
+		public void updateItem(String ballot, boolean empty) {
+			super.updateItem(ballot, empty);
+			setText(null);
+			setGraphic(null);
+			
+			if(ballot != null && !empty) {
+				question.setText("hi");
+				answerList.setValue("Select your choice");
+				//answerList.setItems(null);
+			}
+		}
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		listView.setItems(data);
+		listView.setCellFactory(param -> new Cell());
+	}
 
     public void cancel(javafx.event.ActionEvent event) throws IOException{
         //logout method
@@ -25,4 +63,5 @@ public class VoteController extends ScreenController{
         //cycle through each answer and save each vote into a list (we might want to do this when they change the vote answer, just in case the session messes up.)
         switchScreen(event, "confirm_screen.fxml");
     }
+
 }
