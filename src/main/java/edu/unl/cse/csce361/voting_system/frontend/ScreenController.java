8ce8b0e91f991befc598e9cc77f012533aee98ef
syeap2 <syeap2@huskers.unl.edu>
2020-08-04 14:05:04 -0500

    Solved merge conflict

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/ScreenController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/ScreenController.java
@@ -1,5 +1,7 @@
 package edu.unl.cse.csce361.voting_system.frontend;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -7,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -26,7 +30,8 @@ public abstract class ScreenController {
         window.show();
     }
 
-    public void alertScreen(String title, String message, String message2, String buttonLabel) {
+    /*
+    public void alertScreen(String title, String message, String message2, String noButton, String yesButton) {
 
         Stage window = new Stage();
         //application modality allows the application and screen below to remain open,
@@ -40,16 +45,74 @@ public abstract class ScreenController {
         label.setText(message);
         Label label2 = new Label();
         label2.setText(message2);
-
-        Button tryAgain = new Button(buttonLabel);
+        
+        Button yes = new Button(yesButton);
+        //yes.setOnAction(e -> switchScreen(e, "login.fxml"));
+        yes.setOnAction(e -> {
+			try {
+				FXMLLoader loader = new FXMLLoader();
+		        loader.setLocation(getClass().getResource("login.fxml").toExternalForm());
+		        Parent parent = loader.load();
+				Scene scene = new Scene(parent);
+				Stage stage = (Stage) yes.getScene().getWindow();
+				stage.setScene(scene);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+        });
+        
+        Button tryAgain = new Button(noButton);
         tryAgain.setOnAction(e -> window.close());
-
+        
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(label, label2, tryAgain);
+        layout.getChildren().addAll(label, label2, tryAgain, yes);
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout);
         window.setScene(scene);
         window.showAndWait();
-    }
+    }*/
+    
+    /*
+    public boolean alertScreen(String title, String message, String message2, String noButton, String yesButton) {
+
+    	boolean selectedButton;
+    	
+        Stage window = new Stage();
+        //application modality allows the application and screen below to remain open,
+        //but non-functional until the alert screen closes
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinHeight(150);
+        window.setMinWidth(350);
+
+        Label label = new Label();
+        label.setText(message);
+        Label label2 = new Label();
+        label2.setText(message2);
+        
+        Button yes = new Button(yesButton);
+        //yes.setOnAction(e -> switchScreen(e, "login.fxml"));
+        yes.setOnAction(new EventHandler<ActionEvent>(){
+        	public void handle(ActionEvent e) {
+        		window.close();
+        		return selectedButton = true;
+        	}
+        });
+                
+        Button tryAgain = new Button(noButton);
+        tryAgain.setOnAction(e -> window.close());
+        
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, label2, tryAgain, yes);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+        
+        
+    }*/
+    
 }
--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/VoteController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/VoteController.java
@@ -1,20 +1,20 @@
 package edu.unl.cse.csce361.voting_system.frontend;
 
 import javafx.collections.FXCollections;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+
 import javafx.scene.layout.HBox;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import edu.unl.cse.csce361.voting_system.logic.DataLogic;
 import edu.unl.cse.csce361.voting_system.logic.QuestionAnswer;
 
 public class VoteController extends ScreenController implements Initializable {
-	
-	@FXML
+
 	private ListView<QuestionAnswer> listView;
 
 	static class Cell extends ListCell<QuestionAnswer> {
@@ -22,11 +22,16 @@ public class VoteController extends ScreenController implements Initializable {
 		HBox hbox;
 		Label question = new Label();
 		ChoiceBox<String> answerChoiceList = new ChoiceBox<String>();
+		Button button = new Button("Select");
+		String selectedAnswer;
 		
 		public Cell() {
 			super();
-			hbox = new HBox(question, answerChoiceList);
+			hbox = new HBox(question, answerChoiceList, button);
 			hbox.setSpacing(10);
+			//selectedAnswer = answerChoiceList.getValue();
+			//System.out.println(selectedAnswer);
+			button.setOnAction(e -> System.out.println(answerChoiceList.getValue()));
 		}
 		
 		public void updateItem(QuestionAnswer ballot, boolean empty) {
@@ -41,13 +46,17 @@ public class VoteController extends ScreenController implements Initializable {
 				setGraphic(hbox);
 			}
 		}
+		/*
+		public String getAnswer() {
+			return answerChoiceList.getValue();
+		}*/
 	}
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		List<QuestionAnswer> questionsAndAnswer = DataLogic.getInstance().getAllQuestionsAndAnswers();
 		listView.setItems(FXCollections.observableArrayList(questionsAndAnswer));
-
+		
 		listView.setCellFactory(param -> new Cell());
 	}
 
@@ -56,11 +65,17 @@ public class VoteController extends ScreenController implements Initializable {
         //logout();
         switchScreen(event, "login.fxml");
     }
+    
     public void submitVotes(javafx.event.ActionEvent event) throws IOException{
+    	List<String> question = new ArrayList<>();
         //cycle through each answer and save each vote into a list (we might want to do this when they change the vote answer, just in case the session messes up.)
         //DataLogic.getInstance().submitVote(null);
     	//alertScreen("ARE YOU SURE?", "Be sure to review your answer", "If your answers are correct, click CONFIRM", "GO BACK", "CONFIRM");
     	switchScreen(event, "confirm_screen.fxml");
+    	//question.add(listView.getItems().get(0).getQuestionText());
+    	//question.add(listView);
+
+    	System.out.println(listView);
     }
 
 }
