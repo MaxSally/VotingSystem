49a529e34c75c74b986a46db7718cad4de173814
Max <qnguyen16@huskers.unl.edu>
2020-08-03 19:19:18 -0500

    add alertScreen method to ScreenController

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/ScreenController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/ScreenController.java
@@ -1,9 +1,14 @@
 package edu.unl.cse.csce361.voting_system.frontend;
 
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,5 +24,32 @@ public abstract class ScreenController {
 
         window.setScene(scene);
         window.show();
+    }
+
+    public void alertScreen(String title, String message, String message2, String buttonLabel) {
+
+        Stage window = new Stage();
+        //application modality allows the application and screen below to remain open,
+        //but non-functional until the alert screen closes
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinHeight(150);
+        window.setMinWidth(350);
+
+        Label label = new Label();
+        label.setText(message);
+        Label label2 = new Label();
+        label2.setText(message2);
+
+        Button tryAgain = new Button(buttonLabel);
+        tryAgain.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, label2, tryAgain);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
     }
 }
