8d0dded488e6e948ef96313748955836438292de
syeap2 <syeap2@huskers.unl.edu>
2020-08-05 20:00:22 -0500

    Created an alertScreen when voter want to submit their vote
    - asking them to confirm their choice
    - voter can choose to go back and change their choice
    - voter can process with their choice and navigate to thankYou scene
    - Solved merge conflict when getting the latest file

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/VoteController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/VoteController.java
@@ -3,16 +3,18 @@ package edu.unl.cse.csce361.voting_system.frontend;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 
 import javafx.scene.layout.HBox;
-import javafx.util.Pair;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -21,7 +23,10 @@ import edu.unl.cse.csce361.voting_system.logic.DataLogic;
 import edu.unl.cse.csce361.voting_system.logic.QuestionAnswer;
 
 public class VoteController extends ScreenController implements Initializable {
-
+	
+	@FXML
+	private Label electionName;
+	
 	@FXML
 	private ListView<QuestionAnswer> listView;
 	
@@ -57,10 +62,13 @@ public class VoteController extends ScreenController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		selectedAnswer = new HashMap<>();
+		electionName.setText(DataLogic.getInstance().getCurrentElectionName());
+
 		List<QuestionAnswer> questionsAndAnswer = DataLogic.getInstance().getAllQuestionsAndAnswers();
 		listView.setItems(FXCollections.observableArrayList(questionsAndAnswer));
 		
 		listView.setCellFactory(param -> new Cell());
+		
 	}
 
     public void cancel(javafx.event.ActionEvent event) throws IOException{
@@ -70,9 +78,48 @@ public class VoteController extends ScreenController implements Initializable {
     }
     
     public void submitVotes(javafx.event.ActionEvent event) throws IOException{
-		//TODO modify this method to save the vote information in a local variable and navigate to confirm page. Move submitVote method to confirm screen.
-    	DataLogic.getInstance().submitVote(selectedAnswer);
-    	switchScreen(event, "thankYou_screen.fxml");
+    	alertScreen(event, "Alert!", "Make sure that you have chosen the choice that you want as you wont be able to go back", 
+    			"If everything is good, click 'ok' and click 'Submit Vote'", "Go Back", "Ok");
     }
+    
+    public void alertScreen(javafx.event.ActionEvent event, String title, String message, String message2, String backButton, String confirmButton) {
+
+        Stage window = new Stage();
+        //application modality allows the application and screen below to remain open,
+        //but non-functional until the alert screen closes
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinHeight(150);
+        window.setMinWidth(350);
+
+        Label label = new Label();
+        label.setText(message);
+        Label label2 = new Label();
+        label2.setText(message2);
+        
+        Button confirm = new Button(confirmButton);
+        confirm.setOnAction(e -> {
+			try {
+		    	DataLogic.getInstance().submitVote(selectedAnswer);
+				switchScreen(event, "thankYou_screen.fxml");
+				window.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
+        
+        Button goBack = new Button(backButton);
+        goBack.setOnAction(e -> window.close());
+        
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, label2, goBack, confirm);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
 
 }
