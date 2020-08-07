9945df21406e24a3781eb29fa992733dc289eead
syeap2 <syeap2@huskers.unl.edu>
2020-08-07 14:47:09 -0500

    Implemented table to view a list of voter in auditor screen
    - rebase questionAnswer_backend into questionAnswer_GUI
    - solved merge conflict

---
+++ src/main/java/edu/unl/cse/csce361/voting_system/logic/VoterStatus.java
@@ -0,0 +1,28 @@
+package edu.unl.cse.csce361.voting_system.logic;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class VoterStatus{
+	private SimpleStringProperty name, status;
+	
+	public VoterStatus(String name, String status) {
+        this.name = new SimpleStringProperty(name);
+        this.status = new SimpleStringProperty(status);
+    }
+	
+	public String getName() {
+		return name.get();
+	}
+	
+	public void setName(String name) {
+        this.name = new SimpleStringProperty(name);
+    }
+	
+	public String getStatus() {
+		return status.get();
+	}
+	
+	public void setStatus(String status) {
+        this.status = new SimpleStringProperty(status);
+    }
+}
