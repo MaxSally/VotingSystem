abfa5e92ebcee7da9d34145bef07b54a8b42fad3
syeap2 <syeap2@huskers.unl.edu>
2020-08-07 19:58:34 -0500

    Implemented table to display the results for the ballot
    - created new class "BallotResult" in logic layer

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/AuditorController.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/AuditorController.java
@@ -1,9 +1,11 @@
 package edu.unl.cse.csce361.voting_system.frontend;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import edu.unl.cse.csce361.voting_system.logic.BallotResult;
 import edu.unl.cse.csce361.voting_system.logic.DataLogic;
 import edu.unl.cse.csce361.voting_system.logic.VoterStatus;
 import javafx.collections.FXCollections;
@@ -17,7 +19,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class AuditorController implements Initializable{
 
 	@FXML
-	private TableView<VoterStatus> tableView;
+	private TableView<VoterStatus> voterTable;
 	
 	@FXML
 	private TableColumn<VoterStatus, String> voterName;
@@ -25,11 +27,25 @@ public class AuditorController implements Initializable{
 	@FXML
 	private TableColumn<VoterStatus, String> voterStatus;
 	
-	ObservableList<VoterStatus> voterList = FXCollections.observableArrayList();
+	@FXML
+	private TableView<BallotResult> ballotTable;
 	
+	@FXML
+	private TableColumn<BallotResult, String> questionText;
+	
+	@FXML
+	private TableColumn<BallotResult, String> answerOptionText;
+	
+	@FXML
+	private TableColumn<BallotResult, Long> numberOfVotes;
+	
+	ObservableList<VoterStatus> voterList = FXCollections.observableArrayList();
+	ObservableList<BallotResult> questionWithAnswerResultList = FXCollections.observableArrayList();
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		//Voter and status table
 		Map<String, String> voterWithStatus = DataLogic.getInstance().getAllVoterStatus();
 			
 		for(Map.Entry<String, String> voter : voterWithStatus.entrySet()) {
@@ -39,6 +55,24 @@ public class AuditorController implements Initializable{
 		voterName.setCellValueFactory(new PropertyValueFactory<VoterStatus, String>("name"));
         voterStatus.setCellValueFactory(new PropertyValueFactory<VoterStatus, String>("status"));
 		
-		tableView.setItems(voterList);
+		voterTable.setItems(voterList);
+		
+		//ballot table
+		Map<String, Map<String, Long>> getFinalResult = DataLogic.getInstance().getFinalResult();
+		Map<String, Long> answerChoiceWithResult = new HashMap<>();
+		
+		for(Map.Entry<String, Map<String, Long>> questionWithAnswerResult : getFinalResult.entrySet()) {
+			answerChoiceWithResult = questionWithAnswerResult.getValue();
+			for(Map.Entry<String, Long> answerWithResult : answerChoiceWithResult.entrySet()) {
+				questionWithAnswerResultList.add(DataLogic.getInstance().getBallotResult(questionWithAnswerResult.getKey(), answerWithResult.getKey(), answerWithResult.getValue()));
+			}
+		}
+				
+		questionText.setCellValueFactory(new PropertyValueFactory<BallotResult, String>("questionText"));
+        answerOptionText.setCellValueFactory(new PropertyValueFactory<BallotResult, String>("answerOptionText"));
+        numberOfVotes.setCellValueFactory(new PropertyValueFactory<BallotResult, Long>("votes"));
+
+		ballotTable.setItems(questionWithAnswerResultList);
+
 	}
 }
