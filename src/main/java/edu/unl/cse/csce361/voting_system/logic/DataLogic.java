e83660017d405a1972232030f00ffdb35a4b2ba4
matthew.hedberg <matthew.hedberg@huskers.unl.edu>
2020-08-08 14:45:59 -0500

    Added createNewElection

--- src/main/java/edu/unl/cse/csce361/voting_system/logic/DataLogic.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/logic/DataLogic.java
@@ -2,6 +2,8 @@ package edu.unl.cse.csce361.voting_system.logic;
 
 import edu.unl.cse.csce361.voting_system.backend.*;
 import javafx.util.Pair;
+
+import java.time.LocalDate;
 import java.util.*;
 
 public class DataLogic {
@@ -102,45 +104,88 @@ public class DataLogic {
     }
 
     public String getCurrentVoterName() {
-    	return currentVoter.getName();
+        return currentVoter.getName();
     }
 
     public Map<String, String>getVoterVoteResult() {
         return Backend.getInstance().getVoterVoteResult(currentVoter, currentElection.getElectionName());
     }
-    
+
     public boolean adminLogIn(String name, String password) {
         currentAdmin = Backend.getInstance().adminLogIn(name, password);
         return currentAdmin != null;
     }
-    
+
     public String getCurrentElectionName() {
-    	return currentElection.getName();
+        return currentElection.getName();
     }
-    
+
     public Map<String, String> getAllVoterStatus(){
-    	return Backend.getInstance().getAllVoterStatus(currentAdmin, currentElection.getName());
+        return Backend.getInstance().getAllVoterStatus(currentAdmin, currentElection.getName());
     }
-    
+
     public VoterStatus getVoterAndStatus(String name, String status) {
-    	return new VoterStatus(name, status);
+        return new VoterStatus(name, status);
     }
-    
+
     public Map<String, Map<String, Long>> getFinalResult(){
-    	return Backend.getInstance().getFinalResult(currentAdmin, currentElection.getName());
+        return Backend.getInstance().getFinalResult(currentAdmin, currentElection.getName());
     }
-    
+
     public BallotResult getBallotResult(String questionText, String answerOptionText, Long votes) {
-    	return new BallotResult(questionText, answerOptionText, votes);
+        return new BallotResult(questionText, answerOptionText, votes);
     }
-    
+
     public void setCurrentEditElection(String electionName) {
-    	currentEditElection = Backend.getInstance().getElectionByName(electionName);;
+        currentEditElection = Backend.getInstance().getElectionByName(electionName);;
     }
-    
+
     public String getCurrentEditElectionName() {
-    	return currentEditElection.getName();
+        return currentEditElection.getName();
     }
- 
+
+    public void createNewElection(String electionName, List<QuestionAnswer> questions, LocalDate startTime, LocalDate endTime, boolean status) {
+        if (currentAdmin instanceof ElectionOfficial) {
+            Backend.getInstance().createNewElection((ElectionOfficial) currentAdmin, electionName, startTime, endTime, status);
+
+            for (QuestionAnswer question : questions) {
+                Backend.getInstance().createNewQuestion((ElectionOfficial) currentAdmin, electionName, question.getQuestionText());
+                List<String> answerList = question.getAnswerText();
+                for (String answers : answerList) {
+                    Backend.getInstance().createNewAnswer((ElectionOfficial) currentAdmin, question.getQuestionText(), answers, electionName);
+                }
+            }
+        }
+    }
+    public void updateElection(String electionName, List<QuestionAnswer> questions, LocalDate startTime, LocalDate endTime, boolean status){
+        List<QuestionAnswer> oldQuestions = getAllEditableQuestionsAnswers();
+        if (currentAdmin instanceof ElectionOfficial) {
+            Backend.getInstance().updateElectionName((ElectionOfficial) currentAdmin, currentElection.getName(), electionName);
+
+            for (int i = 0; i < questions.size(); i++ ) {
+                Backend.getInstance().updateQuestion((ElectionOfficial) currentAdmin, electionName, oldQuestions.get(i).getQuestionText(), questions.get(i).getQuestionText());
+
+                //for (int j = 0; j < ) {
+                  //  Backend.getInstance().createNewAnswer((ElectionOfficial) currentAdmin, question.getQuestionText(), answers, electionName);
+              //  }
+            }
+        }
+
+    }
+    public List<QuestionAnswer> getAllEditableQuestionsAnswers() {
+        List<String> questions = Backend.getInstance().getAllQuestionsByElection(currentEditElection.getName());
+        List<QuestionAnswer> lstCurrentQA = new ArrayList<>();
+        for(String question : questions) {
+            List<Pair<String, Long> > answerOptions = Backend.getInstance().getAllAnswersByQuestion(question, currentEditElection.getName());
+            QuestionAndAnswerOption questionAndAnswerOption = new QuestionAndAnswerOption(currentEditElection.getName(), question);
+            questionAndAnswerOption.setAnswerOptions(answerOptions);
+            lstCurrentQA.add(questionAndAnswerOption);
+        }
+        lstQuestionAnswer = lstCurrentQA;
+        return lstCurrentQA;
+    }
+
+
+
 }
 
---
+++ src/main/java/edu/unl/cse/csce361/voting_system/logic/ElectionBuilder.java
@@ -0,0 +1,5 @@
+package edu.unl.cse.csce361.voting_system.logic;
+
+public class ElectionBuilder {
+
+}
