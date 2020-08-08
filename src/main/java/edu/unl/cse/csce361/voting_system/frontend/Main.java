0bb9b8c3b4688c04cd44c66ea8552b42d6e8c94b
syeap2 <syeap2@huskers.unl.edu>
2020-08-07 23:41:24 -0500

    Implement listview for editing election

--- src/main/java/edu/unl/cse/csce361/voting_system/frontend/Main.java
+++ src/main/java/edu/unl/cse/csce361/voting_system/frontend/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("edit_election.fxml"));
         primaryStage.setTitle("Voting!");
         primaryStage.setScene(new Scene(root, 400, 600));
         primaryStage.show();
